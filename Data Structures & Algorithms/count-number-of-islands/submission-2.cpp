e5372a48fcf0f15803b256904ada69682ef0bf81
Vitaly Boronenko <150250096+lolopopkafirst@users.noreply.github.com>
2026-06-05 16:35:32 +0200

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0;
+        for(int i = 0 ; i < grid.size() ; i++){
+            for (int j = 0 ; j < grid[i].size() ; j++){
+                if(grid[i][j] == '1'){
+                    helper(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    void helper(vector<vector<char>>& grid, int i, int j){
+        if(i >= grid.size() || i < 0 || j < 0 || j >= grid[i].size() || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        helper(grid, i + 1, j);
+        helper(grid, i, j + 1);
+        helper(grid, i - 1, j);
+        helper(grid, i, j - 1);
+    }
+};
