84e84c57e9d8a659cf8358e39fd17988793d9406
Vitaly Boronenko <150250096+lolopopkafirst@users.noreply.github.com>
2026-06-08 21:30:25 +0200

    Add: islands-and-treasure - submission-1

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        
+        for(size_t i = 0 ; i < grid.size(); ++i){
+            for(size_t j = 0 ; j < grid[i].size(); ++j){
+                if(grid[i][j] == 0){
+                    helper(grid, i, j, 0);
+                } 
+            }   
+        }
+    }
+    void helper(vector<vector<int>>& grid, size_t row, size_t col, int step){
+        if(row < 0 || row >= grid.size() || col < 0 || col >= grid[row].size() || grid[row][col] == -1 ){
+            return;
+        }
+        if(step != 0 && grid[row][col] == 0){
+            return;
+        }
+        if(step > 0 && step >= grid[row][col]){
+            return;
+        }
+        if(step > 0) {
+            grid[row][col] = step;
+        }
+        helper(grid, row + 1, col, step + 1);
+        helper(grid, row - 1, col, step + 1);
+        helper(grid, row, col - 1, step + 1);
+        helper(grid, row, col + 1, step + 1);
+    
+    }
+};
