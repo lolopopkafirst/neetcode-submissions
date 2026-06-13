849e1c211ac9617d538ac396e9a07550ec04fb3e
Vitaly Boronenko <150250096+lolopopkafirst@users.noreply.github.com>
2026-06-13 16:19:15 +0200

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) { 
+        unordered_map<int, vector<int>> um;
+        
+        for(auto& v : edges){
+
+            um[v[0]].push_back(v[1]);
+            um[v[1]].push_back(v[0]);
+        }
+
+        vector<int> state(n, 0);
+        if(!dfs(um, state, 0, -1)){
+            return false;           
+        }
+        
+        for(int i : state){
+            if(i == 0) return false;
+        }
+        
+        return true;
+    }
+
+    bool dfs(unordered_map<int, vector<int>>& um , vector<int>& state, int curr, int parent){
+        if(state[curr] == 1) return false;
+        state[curr] = 1;
+        for(int next : um[curr]){
+            if(next == parent) continue;
+            if(!dfs(um, state, next, curr)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
