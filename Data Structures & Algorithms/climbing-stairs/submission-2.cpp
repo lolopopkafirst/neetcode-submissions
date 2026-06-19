8a0ab31e972dd1bc6c533ea3ea6e5b0bba1dbf8d
Vitaly Boronenko <150250096+lolopopkafirst@users.noreply.github.com>
2026-06-20 00:54:10 +0200

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       vector<int> arr(n + 3, 0);
+       arr[0] = 1;  
+       for(int i = 0; i < n ; ++i){
+            arr[i + 1] += arr[i];
+            arr[i + 2] += arr[i];
+       }
+       return arr[n];
+    }
+};
