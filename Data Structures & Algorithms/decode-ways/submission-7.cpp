b3618fda89a14cab6d43789efed28589dd59e74e
Vitaly Boronenko <150250096+lolopopkafirst@users.noreply.github.com>
2026-06-25 00:39:22 +0200

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        vector<int> dp(s.size() + 1, 0);
+        if(s[0] == '0') return 0;
+        dp[s.size()] = 1;
+        for(int i = s.size() - 1 ; i >= 0 ; --i) {
+             if(s[i] == '0'){
+                 dp[i] = 0;
+                 continue;
+            }
+            dp[i] = dp[i + 1];
+            int sum = (s[i] - '1' + 1) * 10 + s[i + 1] - '1' + 1;
+            if(i != s.size() - 1 && sum <= 26 && sum > 0){
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+};
