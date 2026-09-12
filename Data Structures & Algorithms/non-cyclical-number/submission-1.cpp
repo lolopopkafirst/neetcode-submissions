0ee1074565574dc4c5a5bebeeb14ae3bf1f79642
Vitaly Boronenko <150250096+lolopopkafirst@users.noreply.github.com>
2026-09-12 20:44:45 +0200

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> memo;
+        return rek(n, memo);
+    }
+    bool rek(int n, unordered_set<int>& memo){
+        if(n == 1) return true;
+        int res = 0;
+        while (n > 0){
+            res += pow(n % 10, 2);
+            n /= 10;
+        }
+        if(memo.find(res) != memo.end()) return false;
+        cout << res << "\n";
+        memo.insert(res);
+        return rek(res, memo);
+    }
+};
